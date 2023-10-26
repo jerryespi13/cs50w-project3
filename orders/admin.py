180a274290416bf636c8b8038ca9483d294c6fc2
jerryespi13 <jerryespi@gmail.com>
2023-10-26 17:28:54 -0600

    creando mas modelos

--- orders/admin.py
+++ orders/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 import orders.models as model
 
 # Register your models here.
-admin.site.register(model.Usuarios)
+admin.site.register(model.Usuario)
+admin.site.register(model.Pizza)
+admin.site.register(model.TamañoPizza)
+admin.site.register(model.TipoPizza)
--- orders/models.py
+++ orders/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Usuarios(models.Model):
+class Usuario(models.Model):
     firstName = models.CharField(max_length=64)
     lastName = models.CharField(max_length=64)
     usuario = models.CharField(max_length=16)
@@ -12,4 +12,28 @@ class Usuarios(models.Model):
     direction = models.CharField(max_length=64)
 
     def __str__(self):
-        return "f{self.firstName} {self.lastName}"
+        return f"{self.firstName} {self.lastName}"
+
+class TamañoPizza(models.Model):
+    nombre = models.CharField(max_length=6)
+    
+    def __str__(self):
+        return f"{self.nombre}"
+
+class TipoPizza(models.Model):
+    nombre = models.CharField(max_length=12)
+
+    def __str__(self):
+        return f"{self.nombre}"
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=16)
+    description = models.CharField(max_length=64)
+    size = models.ForeignKey(TamañoPizza, on_delete=models.CASCADE, related_name="pizza")
+    type = models.ForeignKey(TipoPizza, on_delete=models.CASCADE, related_name="pizza")
+    price = models.FloatField()
+    imagen = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name} {self.type} {self.size}"
+    
