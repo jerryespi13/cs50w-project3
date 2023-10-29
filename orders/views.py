431009bf4780dcf938cff22b1f4e42a793019e28
jerryespi13 <jerryespi@gmail.com>
2023-10-29 00:15:54 -0600

    recepcionando info de los formularios

--- orders/views.py
+++ orders/views.py
@@ -6,7 +6,28 @@ def index(request):
     return render(request, "orders/index.html")
 
 def register(request):
-    return render(request, "orders/register.html")
+    if request.method == "POST":
+        nombre = request.POST["nombre"]
+        apellido = request.POST["apellido"]
+        usuario = request.POST["usuario"]
+        contraseña = request.POST["contraseña"]
+        confirmacion = request.POST["confirmarContraseña"]
+        direccion = request.POST["direccion"]
+        telefono = request.POST["telefono"]
+       
+
+        print(nombre + apellido + usuario + contraseña + confirmacion + direccion + telefono)
+        return render(request, "orders/register.html")
+
+    else:
+        return render(request, "orders/register.html")
 
 def login(request):
-    return render(request, "orders/login.html")
+    if request.method == "POST":
+        usuario = request.POST["usuario"]
+        contraseña = request.POST["contraseña"]
+        print(usuario + contraseña)
+        return render(request, "orders/login.html")
+
+    else:
+        return render(request, "orders/login.html")
