a7d046b8a53bab1517b93fa732c3b65a97adc122
jerryespi13 <jerryespi@gmail.com>
2023-11-02 23:33:23 -0600

    dropdown para toppings

--- orders/static/js/index.js
+++ orders/static/js/index.js
@@ -8,7 +8,6 @@ forEach(link =>{
 });
 
 // dropdown menu 
-
 const dropdowns = document.querySelectorAll('.dropdown');
 dropdowns.forEach(dropdown =>{
     const select = dropdown.querySelector('.select');
@@ -22,7 +21,7 @@ dropdowns.forEach(dropdown =>{
         caret.classList.toggle('caret-rotate');
         menu.classList.toggle('menu-open');
     });
-    options.forEach(option=>{
+    options.forEach(option=>{   
         option.addEventListener('click', ()=>{
             selected.innerText = option.innerText;
             select.classList.remove('select-clicked');
@@ -84,3 +83,31 @@ extras.forEach(extra =>{
         })
     })
 })
+
+// dropdown para toppings
+const dropdownsToppings = document.querySelectorAll('.dropdown-topping');
+dropdownsToppings.forEach(dropdownTopping =>{
+    const select = dropdownTopping.querySelector('.select');
+    const caret = dropdownTopping.querySelector('.caret');
+    const menu = dropdownTopping.querySelector('.topping');
+    const options = dropdownTopping.querySelectorAll('.topping li');
+    const selected = dropdownTopping.querySelector('.selected');
+
+    select.addEventListener('click', ()=>{
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('menu-open');
+    });
+    options.forEach(option=>{   
+        option.addEventListener('click', ()=>{
+            selected.innerText = option.innerText;
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open');
+            options.forEach(option=>{
+                option.classList.remove('active');
+            });
+            option.classList.add('active');
+        });
+    });
+});
