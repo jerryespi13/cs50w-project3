a7885b0b518012ffc64cbc6eb5263beee62c0bb8
jerryespi13 <jerryespi@gmail.com>
2023-11-10 15:27:37 -0600

    ver productos del pedido en panel de administracion

--- orders/admin.py
+++ orders/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 import orders.models as model
 from.models import User
 
+
+class OrdenProductoInline(admin.TabularInline):
+    model = model.OrdenProducto
+    extra = 0
+
+
+    
+
+class OrdenAdmin(admin.ModelAdmin):
+    inlines = [OrdenProductoInline]
+    list_display=('fecha', 'estado', 'usuario', 'total')
+    list_editable=('estado',)
+
 # Register your models here.
 #admin.site.register(model.Tamaño)
 admin.site.register(User)
@@ -12,6 +25,6 @@ admin.site.register(model.Extra)
 admin.site.register(model.DinnerPlate)
 admin.site.register(model.Salads)
 admin.site.register(model.Pasta)
-admin.site.register(model.Orden)
+admin.site.register(model.Orden, OrdenAdmin)
 admin.site.register(model.OrdenProducto)
 admin.site.register(model.Tamaño)
